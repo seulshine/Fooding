3d50788ea1a9fab60aa7e951613c45605c499217
IndongLee <kundera@kakao.com>
2020-04-22 17:17:03 +0900

    [Add] datetime picker dialog

--- front/app/src/main/java/com/example/food/ui/MainFragment.kt
+++ front/app/src/main/java/com/example/food/ui/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.food.ui
 
+import android.app.DatePickerDialog
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,8 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.food.R
+import kotlinx.android.synthetic.main.main_fragment.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
+    private lateinit var selectedDate: String
+    private val calendar by lazy { Calendar.getInstance() }
 
     companion object {
         fun newInstance() = MainFragment()
@@ -27,7 +33,43 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        tv_main_datetime_picker.text = getStringDate()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            calendar.let {
+                it[Calendar.YEAR] = year
+                it[Calendar.MONTH] = month
+                it[Calendar.DAY_OF_MONTH] = dayOfMonth
+            }
+            tv_main_datetime_picker.text = getStringDate()
+        }
+
+        tv_main_datetime_picker.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                dateSetListener,
+                calendar[Calendar.YEAR],
+                calendar[Calendar.MONTH],
+                calendar[Calendar.DAY_OF_MONTH]
+            ).show()
+        }
+
+        img_main_date_prev.setOnClickListener {
+            changeCalendar(-1)
+        }
+        img_main_date_next.setOnClickListener {
+            changeCalendar(1)
+        }
+    }
+
+    private fun getStringDate(): String = SimpleDateFormat("yyyy-MM-dd").format(calendar.time)
+
+    private fun changeCalendar(dayDifference: Int) {
+        calendar.let {
+            it.add(Calendar.DAY_OF_MONTH, dayDifference)
+        }
+        tv_main_datetime_picker.text = getStringDate()
     }
 
 }
